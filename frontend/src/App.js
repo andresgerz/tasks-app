f4f3db0a3b2aa51461f6ad128ba60c81a907bb68
andresger <andres.gerz@gmail.com>
2019-09-04 22:15:02 -0300

    I added axios to my app and I made my component CreateUser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,11 +14,12 @@ function App() {
       Hello World!!
       
       <Navigation />
-
-      <Route path="/" exact component={NotesList} />
-      <Route path="/edit/:id" component={CreateNote} />
-      <Route path="/create" component={CreateNote} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container p-4">
+        <Route path="/" exact component={NotesList} />
+        <Route path="/edit/:id" component={CreateNote} />
+        <Route path="/create" component={CreateNote} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
